948348a06fab77261a118c4f0740319f55f88003
Bartosz Flis <bartosz@zoetrope.io>
2017-08-09 13:47:37 +0100

    run_docker.sh: use full path to docker-compose

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,8 @@
 #! /bin/bash -e
 
 docker build -f Dockerfile.stretch.zcsimbase -t zcsimbase:latest .
-docker-compose build
-docker-compose up
+.env/bin/docker-compose build
+.env/bin/docker-compose up
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker volume rm $(docker volume ls --format '{{.Name}}')
