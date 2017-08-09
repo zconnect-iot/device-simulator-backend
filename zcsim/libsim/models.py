008930582d9cf9bb1453f75a0c2e39df6da3a641
Bartosz Flis <bartosz@zoetrope.io>
2017-08-09 14:18:02 +0100

    libsim: add proper handling of arrays returned by solver

--- zcsim/libsim/models.py
+++ zcsim/libsim/models.py
@@ -48,28 +48,9 @@ class FirstOrderVar(T(
         # TODO: divide the timestamps in a smarter way
         ts = numpy.linspace(0, sim_step.duration, 100)
         xs = odeint(model, sim_step.x0, ts)
-        return (ts, xs)
+        return (ts, tuple(el for el in xs[:, 0]))
 
-
-def angular_velocity(name):
-    """
-    @param power_in
-    @param efficiency
-    """
-    gain = 0.3
-    time_constant = 10
-
-    def fuse(power_in, efficiency):
-        return power_in * efficiency
-
-    return FirstOrderVar(name, fuse, gain, time_constant)
-
-
-def flow(name):
-    gain = 1
-    time_constant = 0.1
-
-    def fuse(flow, valve_on):
-        return flow if valve_on else 0
-
-    return FirstOrderVar(name, fuse, gain, time_constant)
+def latest_sample(sim_result):
+    ts, xs = sim_result
+    # TODO: Change this when adding higher order variables
+    return xs[-1]
