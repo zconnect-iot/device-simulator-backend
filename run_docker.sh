6eb91a90068aca9b4ce716075167b460f0cbd7b1
Bartosz Flis <bartosz@zoetrope.io>
2017-08-09 13:46:53 +0100

    run_docker.sh: add cleanup

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,8 @@
-#! /bin/bash
+#! /bin/bash -e
 
 docker build -f Dockerfile.stretch.zcsimbase -t zcsimbase:latest .
 docker-compose build
 docker-compose up
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker volume rm $(docker volume ls --format '{{.Name}}')
