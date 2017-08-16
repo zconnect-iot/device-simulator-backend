2e04a7a85b29844b272fdf90df19977242edaa92
Bartosz Flis <bartosz@zoetrope.io>
2017-08-16 13:29:49 +0100

    settings: fully working automatic door model

--- zcsim/settings/systems/automatic_door.py
+++ zcsim/settings/systems/automatic_door.py
@@ -1,10 +1,16 @@
 from collections import namedtuple as T
-from libsim import (models, util)
+from libsim import models
+from libsim.util import (
+    within_bottom_margin,
+    within_top_margin,
+    predicate,
+)
+from functools import partial
 
 motor_efficiency = models.Property(name='motor_efficiency', unit='%', start=70)
-fuse_blown = models.Property(name='fuse_blown', unit='[yes/no]', start=0)
+fuse_replaced = models.Property(name='fuse_replaced', unit='[0/1]', start=0)
 people_waiting = models.Property(
-    name='people_waiting', unit='[yes/no]', start=0
+    name='people_waiting', unit='[0/1]', start=0
 )
 bearing_condition = T('BearingConditon', 'left right')(
     left=models.Property(name='bearing_condition_left', unit='%', start=100),
@@ -12,15 +18,15 @@ bearing_condition = T('BearingConditon', 'left right')(
 )
 
 
-def door_position_input(door_position_now, people_waiting, bearing_condition,
-                        current_in):
+current_in_red_zone = predicate(partial(within_top_margin, 0, 5, 0.05))
+current_in_green_zone = ~current_in_red_zone
+
+
+def door_position_input(people_waiting, bearing_condition):
     """
     It should take from 2 to 30 seconds for the doors to almost close. Remaining
     error could be treated as sensor tolerance.
     """
-    # If insufficient engine power, don't move the door
-    if not util.within_top_margin(0, 5, 0.02, current_in):
-        return door_position_now, 0.001, 1
     # linear transformation (0,100) -> (2,30)
     time_constant = (-28.0 * float(bearing_condition) + 3000.0) / 100.0
     gain = 10
@@ -31,14 +37,20 @@ def door_position_input(door_position_now, people_waiting, bearing_condition,
 
 
 door_position = T('DoorPosition', 'left right')(
-    left=models.FirstOrder(
-        name='door_position_left', start=0,
-        fuse_inputs=door_position_input
+    left=models.PauseWhen(
+        cond=current_in_green_zone,
+        var=models.FirstOrder(
+            name='door_position_left', start=0,
+            fuse_inputs=door_position_input
+        )
     ),
-    right=models.FirstOrder(
-        name='door_position_right', start=0,
-        fuse_inputs=door_position_input
-    ),
+    right=models.PauseWhen(
+        cond=current_in_green_zone,
+        var=models.FirstOrder(
+            name='door_position_right', start=0,
+            fuse_inputs=door_position_input
+        )
+    )
 )
 
 
@@ -46,37 +58,80 @@ def current_in_inputs(door_position, people_waiting):
     gain = 5
     time_constant = 0.2
     door_should_open = people_waiting\
-        and not util.within_top_margin(0, 10, 0.05, door_position)
+        and not within_top_margin(0, 10, 0.05, door_position)
     door_should_close = not people_waiting\
-        and not util.within_bottom_margin(0, 10, 0.05, door_position)
+        and not within_bottom_margin(0, 10, 0.05, door_position)
     should_spin = door_should_open or door_should_close
     return (gain, time_constant, 1) if should_spin\
         else (gain, time_constant, 0)
 
 
-current_in_left = models.FirstOrder(
-    name='current_in_left',
-    start=0,
-    fuse_inputs=current_in_inputs
+current = T('Current', 'left right')(
+    left=models.ResetWhen(
+        cond=lambda fuse_blown: fuse_blown,
+        var=models.Bounded(
+            min=0, max=5,
+            var=models.FirstOrder(
+                name='current_in_left',
+                start=0,
+                fuse_inputs=current_in_inputs
+            )
+        )
+    ),
+    right=models.ResetWhen(
+        cond=lambda fuse_blown: fuse_blown,
+        var=models.Bounded(
+            min=0, max=5,
+            var=models.FirstOrder(
+                name='current_in_right',
+                start=0,
+                fuse_inputs=current_in_inputs
+            )
+        )
+    )
 )
 
 
-current_in_right = models.FirstOrder(
-    name='current_in_right',
-    start=0,
-    fuse_inputs=current_in_inputs
+overheat = T('Overheat', 'left right')(
+    left=models.ResetWhen(
+        cond=current_in_green_zone,
+        var=models.Counter(
+            name='overheat_countdown_left', start=20, step=-1
+        ),
+    ),
+    right=models.ResetWhen(
+        cond=current_in_green_zone,
+        var=models.Counter(
+            name='overheat_countdown_right', start=20, step=-1
+        ),
+    )
 )
 
+
+@predicate
+def is_fuse_blown(x0, inputs):
+    overheat_left, overheat_right, fuse_replaced = inputs
+    if x0:
+        return not fuse_replaced
+    else:
+        return (overheat_left <= 0 or overheat_right <= 0)
+
+
+fuse_blown = models.Boolean(
+    name='fuse_blown',
+    predicate=is_fuse_blown,
+    start=0
+)
 
 system = models.System(**{
     'processes': (
-        door_position.left,
-        door_position.right,
-        current_in_left,
-        current_in_right
+        door_position.left, door_position.right,
+        current.left, current.right,
+        overheat.left, overheat.right,
+        fuse_blown,
     ),
     'properties': (
-        fuse_blown,
+        fuse_replaced,
         motor_efficiency,
         people_waiting,
         bearing_condition.left,
@@ -84,14 +139,15 @@ system = models.System(**{
     ),
     'dependencies': {
         door_position.left: (
-            door_position.left, people_waiting, bearing_condition.left,
-            current_in_left
+            current.left, people_waiting, bearing_condition.left
         ),
         door_position.right: (
-            door_position.right, people_waiting, bearing_condition.right,
-            current_in_right
+            current.right, people_waiting, bearing_condition.right,
         ),
-        current_in_left: (door_position.left, people_waiting),
-        current_in_right: (door_position.right, people_waiting)
+        current.left: (fuse_blown, door_position.left, people_waiting),
+        current.right: (fuse_blown, door_position.right, people_waiting),
+        overheat.left: (current.left,),
+        overheat.right: (current.right,),
+        fuse_blown: (overheat.left, overheat.right, fuse_replaced),
     }
 })
